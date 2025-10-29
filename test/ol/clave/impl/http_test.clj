71d5f338701b900af65eff1b0a4c110609517a29
ramblurr <casey@outskirtslabs.com>
2025-10-29 19:37:02 +0100

    add tests for header normalization

--- test/ol/clave/impl/http_test.clj
+++ test/ol/clave/impl/http_test.clj
@@ -3,7 +3,7 @@
    [ol.clave.impl.commands :as commands]
    [ol.clave.impl.http :as http]
    [ol.clave.impl.test-util :refer [http-client-opts] :as util]
-   [clojure.test :refer [deftest is use-fixtures]]))
+   [clojure.test :refer [deftest is testing use-fixtures]]))
 
 ((requiring-resolve 'hashp.install/install!))
 
@@ -14,3 +14,22 @@
         [session _] (commands/load-directory session)
         [_ nonce] (http/get-nonce session {:cancel-token nil})]
     (is (string? nonce))))
+
+(deftest header-normalization-case-insensitive
+  (testing "normalize-headers lowercases keys"
+    (let [normalized (@#'ol.clave.impl.http/normalize-headers {"Replay-Nonce" "abc"})]
+      (is (= {"replay-nonce" "abc"} normalized)))))
+
+(deftest header-normalization-seq-input
+  (testing "normalize-headers handles sequence of pairs"
+    (let [normalized (@#'ol.clave.impl.http/normalize-headers [["Replay-Nonce" "abc"]
+                                                               [:cache-control "no-store"]])]
+      (is (= {"replay-nonce" "abc"
+              "cache-control" "no-store"}
+             normalized)))))
+
+(deftest get-header-case-insensitive
+  (testing "get-header works regardless of header case"
+    (let [resp {:headers {"replay-nonce" "abc"}}]
+      (is (= "abc" (http/get-header resp "Replay-Nonce")))
+      (is (= "abc" (http/get-header resp "replay-nonce"))))))

f9fa9490c8190468363d345a6409d79f6838c884
Casey Link <casey@outskirtslabs.com>
2025-12-22 13:55:11 +0100

    update flake

--- flake.nix
+++ flake.nix
@@ -2,57 +2,35 @@
   description = "dev env";
   inputs = {
     nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1"; # tracks nixpkgs unstable branch
-    flakelight.url = "github:nix-community/flakelight";
-    flakelight.inputs.nixpkgs.follows = "nixpkgs";
+    devshell.url = "github:numtide/devshell";
+    devenv.url = "https://flakehub.com/f/ramblurr/nix-devenv/*";
+    devenv.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs =
     {
       self,
-      flakelight,
+      devenv,
+      devshell,
       ...
     }:
-    flakelight ./. {
-
+    devenv.lib.mkFlake ./. {
+      withOverlays = [
+        devshell.overlays.default
+        devenv.overlays.default
+      ];
       devShell =
         pkgs:
-        let
-          javaVersion = "25";
-          jdk = pkgs."jdk${javaVersion}";
-          clojure = pkgs.clojure.override { inherit jdk; };
-          libraries = [ ];
-        in
-        {
-          packages = [
-            clojure
-            jdk
-            pkgs.cfssl
-            pkgs.pebble
-            pkgs.clojure-lsp
-            pkgs.clj-kondo
-            pkgs.cljfmt
-            pkgs.babashka
-            pkgs.bbin
-            pkgs.git
+        pkgs.devshell.mkShell {
+          imports = [
+            devenv.capsules.base
+            devenv.capsules.clojure
           ];
-          env.LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath libraries;
-          shellHook = ''
-            if ! command -v clj-nrepl-eval &> /dev/null; then
-              bbin install https://github.com/bhauman/clojure-mcp-light.git --tag v0.2.1 --as clj-nrepl-eval --main-opts '["-m" "clojure-mcp-light.nrepl-eval"]'
-            fi
-            if ! command -v clj-paren-repair &> /dev/null; then
-              bbin install https://github.com/bhauman/clojure-mcp-light.git --tag v0.2.1 --as clj-paren-repair --main-opts '["-m" "clojure-mcp-light.paren-repair"]'
-            fi
-            mkdir -p extra/
-            pushd extra/
-            test -f seed.bb && bb ./seed.bb
-            popd
-          '';
-        };
+          # https://numtide.github.io/devshell
+          commands = [
+            { package = pkgs.cfssl; }
+            { package = pkgs.pebble; }
+          ];
 
-      flakelight.builtinFormatters = false;
-      formatters = pkgs: {
-        "*.nix" = "${pkgs.nixfmt}/bin/nixfmt";
-        "*.clj" = "${pkgs.cljfmt}/bin/cljfmt fix";
-      };
+        };
     };
 }

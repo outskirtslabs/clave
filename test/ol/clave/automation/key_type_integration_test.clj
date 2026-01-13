9f59f5558bba81d4b237ba68c5bee654b175eb4d
Casey Link <casey@outskirtslabs.com>
2026-01-13 15:50:56 +0100

    add key type variations integration tests (test #199)
    - Added new test file: key_type_integration_test.clj with 4 tests
    - Tests verify certificates are issued with correct key types:
      - :p256 produces EC P-256 keys (256 bits)
      - :p384 produces EC P-384 keys (384 bits)
      - :rsa2048 produces RSA 2048-bit keys
      - :rsa4096 produces RSA 4096-bit keys
    - Tests use Java security interfaces for key inspection
    - Tests passing: bb test:parallel (411 tests, 0 failures)
    - Updated prompts/feature_list.json: marked test #199 as passing

---
+++ test/ol/clave/automation/key_type_integration_test.clj
@@ -0,0 +1,126 @@
+(ns ol.clave.automation.key-type-integration-test
+  "Integration tests for key type variations in certificate issuance.
+  Tests run against Pebble ACME test server."
+  (:require
+   [clojure.test :refer [deftest is testing use-fixtures]]
+   [ol.clave.automation :as automation]
+   [ol.clave.acme.challenge :as challenge]
+   [ol.clave.impl.pebble-harness :as pebble]
+   [ol.clave.specs :as specs]
+   [ol.clave.storage.file :as file-storage])
+  (:import
+   [java.nio.file Files]
+   [java.nio.file.attribute FileAttribute]
+   [java.security.interfaces ECPrivateKey RSAPrivateKey]
+   [java.security.spec ECParameterSpec]
+   [java.util.concurrent TimeUnit]))
+
+;; Use :each to give each test a fresh Pebble instance with clean state.
+(use-fixtures :each pebble/pebble-challenge-fixture)
+
+(defn- temp-storage-dir
+  "Creates a temporary directory for storage tests."
+  []
+  (let [path (Files/createTempDirectory "clave-key-type-test-" (make-array FileAttribute 0))]
+    (.toString path)))
+
+(defn- create-solver
+  "Creates an HTTP-01 solver for pebble challenge test server."
+  []
+  {:present (fn [_lease chall account-key]
+              (let [token (::specs/token chall)
+                    key-auth (challenge/key-authorization chall account-key)]
+                (pebble/challtestsrv-add-http01 token key-auth)
+                {:token token}))
+   :cleanup (fn [_lease _chall state]
+              (pebble/challtestsrv-del-http01 (:token state))
+              nil)})
+
+(defn- get-ec-curve-size
+  "Returns the bit size of an EC private key's curve.
+  P-256 returns 256, P-384 returns 384."
+  [^ECPrivateKey key]
+  (let [^ECParameterSpec params (.getParams key)]
+    (.getFieldSize (.getField (.getCurve params)))))
+
+(defn- get-rsa-key-size
+  "Returns the bit size of an RSA private key."
+  [^RSAPrivateKey key]
+  (.bitLength (.getModulus key)))
+
+(defn- obtain-cert-with-key-type
+  "Obtains a certificate with the specified key type and returns the bundle.
+  Returns the certificate bundle or nil if the operation failed."
+  [key-type domain-suffix]
+  (let [storage-dir (temp-storage-dir)
+        storage-impl (file-storage/file-storage storage-dir)
+        ;; Use a unique domain for each key type to avoid conflicts
+        domain (str "keytype-" (name key-type) "-" domain-suffix ".localhost")
+        solver (create-solver)
+        config {:storage storage-impl
+                :issuers [{:directory-url (pebble/uri)}]
+                :solvers {:http-01 solver}
+                :http-client pebble/http-client-opts
+                :skip-domain-validation true
+                :key-type key-type}
+        system (automation/start config)]
+    (try
+      (let [queue (automation/get-event-queue system)]
+        (automation/manage-domains system [domain])
+        ;; Consume domain-added event
+        (.poll queue 5 TimeUnit/SECONDS)
+        ;; Wait for certificate obtain
+        (let [cert-event (.poll queue 30 TimeUnit/SECONDS)]
+          (when (= :certificate-obtained (:type cert-event))
+            (automation/lookup-cert system domain))))
+      (finally
+        (automation/stop system)))))
+
+(deftest key-type-p256-produces-ec-p256-certificate
+  (testing "Certificate with :key-type :p256 has EC P-256 private key"
+    (let [bundle (obtain-cert-with-key-type :p256 "p256")]
+      (is (some? bundle) "Certificate bundle should be obtained")
+      (when bundle
+        (let [^java.security.PrivateKey private-key (:private-key bundle)]
+          (is (some? private-key) "Bundle should have private key")
+          (is (instance? ECPrivateKey private-key)
+              "Private key should be EC key")
+          (is (= 256 (get-ec-curve-size private-key))
+              "EC key should be P-256 (256 bits)"))))))
+
+(deftest key-type-p384-produces-ec-p384-certificate
+  (testing "Certificate with :key-type :p384 has EC P-384 private key"
+    (let [bundle (obtain-cert-with-key-type :p384 "p384")]
+      (is (some? bundle) "Certificate bundle should be obtained")
+      (when bundle
+        (let [^java.security.PrivateKey private-key (:private-key bundle)]
+          (is (some? private-key) "Bundle should have private key")
+          (is (instance? ECPrivateKey private-key)
+              "Private key should be EC key")
+          (is (= 384 (get-ec-curve-size private-key))
+              "EC key should be P-384 (384 bits)"))))))
+
+(deftest key-type-rsa2048-produces-rsa-2048-certificate
+  (testing "Certificate with :key-type :rsa2048 has RSA 2048-bit private key"
+    (let [bundle (obtain-cert-with-key-type :rsa2048 "rsa2048")]
+      (is (some? bundle) "Certificate bundle should be obtained")
+      (when bundle
+        (let [^java.security.PrivateKey private-key (:private-key bundle)]
+          (is (some? private-key) "Bundle should have private key")
+          (is (instance? RSAPrivateKey private-key)
+              "Private key should be RSA key")
+          (is (= 2048 (get-rsa-key-size private-key))
+              "RSA key should be 2048 bits"))))))
+
+(deftest key-type-rsa4096-produces-rsa-4096-certificate
+  (testing "Certificate with :key-type :rsa4096 has RSA 4096-bit private key"
+    (let [bundle (obtain-cert-with-key-type :rsa4096 "rsa4096")]
+      (is (some? bundle) "Certificate bundle should be obtained")
+      (when bundle
+        (let [^java.security.PrivateKey private-key (:private-key bundle)]
+          (is (some? private-key) "Bundle should have private key")
+          (is (instance? RSAPrivateKey private-key)
+              "Private key should be RSA key")
+          (is (= 4096 (get-rsa-key-size private-key))
+              "RSA key should be 4096 bits"))))))
+
